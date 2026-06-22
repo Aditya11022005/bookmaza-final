9c22971ef5a9e0f2942026ff746f3d8714081b78
void <Aditya11022005>
2026-06-23 00:10:50 +0530

    fix pdf worker version mismatch

--- client/src/components/EbookReader.jsx
+++ client/src/components/EbookReader.jsx
@@ -23,7 +23,7 @@ import { toast } from 'sonner';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.mjs';
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const EbookReader = () => {
   const { user } = useAuthStore();
